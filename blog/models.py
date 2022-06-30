dc0d40afb512af8c8e40f705ea2e6ad3ad500254
Yevhenii Olegovych <pivenevgeniy@ukr.net>
2022-06-30 01:01:19 +0000

    update v2

---
+++ blog/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+  title = models.CharField('Title', max_length=100, null=True, blank=True)
+  summary = models.TextField('Summary', blank=True)
+  content = models.TextField('Content', blank=True)
+  slug = models.SlugField(unique=True)
+  created_at = models.DateTimeField('Created At', auto_now_add=True)
+  modified_at = models.DateTimeField('Modified At', auto_now=True)
+  published_at = models.DateTimeField('Published At' null=True, blank=True)
+  
