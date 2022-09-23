a09a008841d21c0373df4ab5de61709772eb7e1b
Yevhenii <pivenevgeniy@ukr.net>
2022-09-23 20:54:37 +0300

    fix project launch

--- blog/models.py
+++ blog/models.py
@@ -7,7 +7,7 @@ class Post(models.Model):
   summary = models.TextField('Summary', blank=True)
   content = models.TextField('Content', blank=True)
   slug = models.SlugField(unique=True)
-  created_at = models.DateTimeField('Created At', auto_now_add=True, default=django.utils.timezone.now)
+  created_at = models.DateTimeField('Created At', auto_now_add=True)
   modified_at = models.DateTimeField('Modified At', auto_now=True)
   published_at = models.DateTimeField('Published At', null=True, blank=True)
   
