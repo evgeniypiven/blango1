15264882dcd4e8cb83b6eda7bf559b8d62c43e5c
Yevhenii <pivenevgeniy@ukr.net>
2022-09-25 21:10:16 +0300

    finish viewsets

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,13 +1,25 @@
 from rest_framework import generics
-from blog.api.serializers import PostSerializer
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from blog.api.serializers import PostSerializer, PostDetailSerializer
 from blog.api.permissions import AuthorModifyOrReadOnly, IsAdminUserForObject
 from blog.models import Post
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
+    permission_classes = [AuthorModifyOrReadOnly |IsAdminUserForObject]
     queryset = Post.objects.all()
-    serializer_class = PostSerializer
+    def get_serializer_class(self):
+        if self.action in ("list", "create"):
+            return PostSerializer
+        return PostDetailSerializer
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    permission_classes = [AuthorModifyOrReadOnly | IsAdminUserForObject]
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+
+class TagViewSet(viewsets.ModelViewSet):
+    # existing attributes omitted
+    @action(methods=["get"], detail=True, name="Posts with the Tag")
+    def posts(self, request, pk=None):
+        tag = self.get_object()
+        post_serializer = PostSerializer(
+        tag.posts, many=True, context={"request": request}
+        )
+    return Response(post_serializer.data)
