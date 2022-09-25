ce847a5686f764a10bd9ab08d30be80eae9ef9dc
Yevhenii <pivenevgeniy@ukr.net>
2022-09-25 14:42:22 +0300

    update post migrations

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -15,7 +15,13 @@ class Migration(migrations.Migration):
             name='Post',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(blank=True, max_length=100, null=True, verbose_name='Title')),
+                ('summary', models.TextField(blank=True, verbose_name='Summary')),
+                ('content', models.TextField(blank=True, verbose_name='Content')),
                 ('slug', models.SlugField(unique=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True, verbose_name='Created At')),
+                ('modified_at', models.DateTimeField(auto_now=True, verbose_name='Modified At')),
+                ('published_at', models.DateTimeField(blank=True, null=True, verbose_name='Published At')),
             ],
         ),
     ]
