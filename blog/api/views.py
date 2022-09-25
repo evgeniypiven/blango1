2a8be33d04a2ac23afa1713a28ecaeaf5970b2e0
Yevhenii <pivenevgeniy@ukr.net>
2022-09-25 21:10:50 +0300

    finish viewsets

--- blog/api/views.py
+++ blog/api/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics
+from rest_framework import generics, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from blog.api.serializers import PostSerializer, PostDetailSerializer
@@ -22,4 +22,4 @@ class TagViewSet(viewsets.ModelViewSet):
         post_serializer = PostSerializer(
         tag.posts, many=True, context={"request": request}
         )
-    return Response(post_serializer.data)
+        return Response(post_serializer.data)
