f7b7b27346f8c3876bbebc1df7439fe66d7b08fe
Yevhenii <pivenevgeniy@ukr.net>
2022-09-27 22:35:23 +0300

    Finish fetch and React hooks

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
 def post_table(request):
-    return render(request, "blog/post-table.html")
+    return render(
+        request, "blog/post-table.html", {"post_list_url": reverse("post-list")}
+    )
